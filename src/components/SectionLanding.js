41a603e8a9b18cf56ba2611c6b37bb41aaae1dc5
rivera-bl <rivera.blo@protonmail.com>
2021-04-21 18:16:59 -0400

    Add further styled-components to About Section

--- src/components/SectionLanding.js
+++ src/components/SectionLanding.js
@@ -4,28 +4,29 @@ import styled from 'styled-components';
 const SectionLanding = () => {
     return (
         <About>
-            <div className="description">
+            <Description>
                 <div className="title">
-                    <div className="hide">
+                    <Hide>
                         <h2>We work to make</h2>
-                    </div>
-                    <div className="hide">
+                    </Hide>
+                    <Hide>
                         <h2>your <span>dreams</span></h2>
-                    </div>
-                    <div className="hide">
+                    </Hide>
+                    <Hide>
                         <h2>come true</h2>
-                    </div>
+                    </Hide>
                 </div>
                 <p>Contact us for any lorem ipsum</p>
                 <button>Contact Us</button>
-            </div>
-                <div className="image">
+            </Description>
+                <Image>
                     <img src={home1} alt="guy-camera"/>
-                </div>
+                </Image>
         </About>
     );
 };
 
+// styled-components
 const About = styled.div`
     min-height: 90vh;
     display: flex;
@@ -33,6 +34,28 @@ const About = styled.div`
     justify-content: space-between;
     padding: 5rem 10rem;
     color: white;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
 `;
 
 export default SectionLanding;
