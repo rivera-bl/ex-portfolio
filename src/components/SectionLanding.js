33a96e27cd9d26837c38f9b4cf06016adb6e9b4d
rivera-bl <rivera.blo@protonmail.com>
2021-04-21 17:38:24 -0400

    Add styled-component for About Section

--- src/components/SectionLanding.js
+++ src/components/SectionLanding.js
@@ -1,8 +1,9 @@
-import home1 from '../img/home1.png'
+import home1 from '../img/home1.png';
+import styled from 'styled-components';
 
 const SectionLanding = () => {
     return (
-        <div>
+        <About>
             <div className="description">
                 <div className="title">
                     <div className="hide">
@@ -17,12 +18,21 @@ const SectionLanding = () => {
                 </div>
                 <p>Contact us for any lorem ipsum</p>
                 <button>Contact Us</button>
+            </div>
                 <div className="image">
                     <img src={home1} alt="guy-camera"/>
                 </div>
-            </div>
-        </div>
+        </About>
     );
-}
+};
+
+const About = styled.div`
+    min-height: 90vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5rem 10rem;
+    color: black;
+`;
 
 export default SectionLanding;
