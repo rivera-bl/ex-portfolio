06f9302021a0a3e087249278aba63acd8b66dd14
rivera-bl <rivera.blo@protonmail.com>
2021-04-21 21:12:08 -0400

    Add styled-components to Faq Section

--- src/components/SectionFaq.js
+++ src/components/SectionFaq.js
@@ -1,6 +1,9 @@
+import styled from 'styled-components';
+import { Container } from '../styles.js';
+
 const SectionFaq = () => {
     return(
-        <div class="faq">
+        <Faq>
             <h2>Any Questions <span>FAQ</span></h2>
             <div className="question">
                 <h4>How do I start?</h4>
@@ -11,6 +14,7 @@ const SectionFaq = () => {
                         hic in.
                     </p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Daily schedule</h4>
@@ -21,6 +25,7 @@ const SectionFaq = () => {
                         hic in.
                     </p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Payment Methods</h4>
@@ -31,6 +36,7 @@ const SectionFaq = () => {
                         hic in.
                     </p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>What products do you offer</h4>
@@ -41,9 +47,37 @@ const SectionFaq = () => {
                         hic in.
                     </p>
                 </div>
+                <div className="faq-line"></div>
             </div>
-        </div>
+        </Faq>
     )
 }
+
+const Faq = styled(Container)`
+  display: block;
+  span {
+    display: block;
+  }
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .faq-line {
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+  .answer {
+    padding: 2rem 0rem;
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+`;
 
 export default SectionFaq;
